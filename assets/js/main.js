67cf7aad089feb30c05e6ef881131cc77278414e
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-10-18 12:22:11 -0300

    Desenvolvido navbar tablet / mobile

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const navMenu = document.querySelector('#nav-menu')
+const navToggle = document.querySelector('#nav-toggle')
+const navClose = document.querySelector('#nav-close')
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.querySelector('#nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
