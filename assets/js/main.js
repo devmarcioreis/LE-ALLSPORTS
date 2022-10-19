bbfc4df0d54ef5a01b716372f3753886e1cf33e5
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-10-19 16:41:16 -0300

    Media Query dispositivos grandes

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,5 @@ const scrollHeader = () => {
     this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
